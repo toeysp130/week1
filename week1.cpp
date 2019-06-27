f082ca1f19c3540a16154dbce4932cf9c9774706
toeysp130 <49556609+toeysp130@users.noreply.github.com>
2019-06-27 23:12:15 +0700

    Create week1.cpp

---
+++ week1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	cout << " Student Code " <<":" << " 6206021612059 " << endl;
+	cout << " Student Name " <<":" << " Watcharakorn Choptum" << endl;
+	cout << "     Division " <<":" << " Information Technology" << endl;
+	cout << "         Year " <<":" << " 1 " << endl; 
+	cout << "         Room " <<":" << " RC" << endl;
+	cout << "          Sex " <<":" << " Male" << endl;
+	cout << "          Age " <<":" << " 18" << endl;
+	return(0);
+}
